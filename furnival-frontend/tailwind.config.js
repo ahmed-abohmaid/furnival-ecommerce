0af39793091978d4d8e5a8a3c49f9ce01e6f481d
AbdelrahmanKaled12 <abdelrahman.khaled1086@gmail.com>
2023-08-02 19:13:46 +0300

    make the container

--- furnival-frontend/tailwind.config.js
+++ furnival-frontend/tailwind.config.js
@@ -11,6 +11,15 @@ module.exports = {
         darkGray: '#3C3C3D',
         lightGreen: '#8FC83D',
       },
+      container: {
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+          '2xl': '6rem',
+        },
+      },
     },
   },
   plugins: [],
