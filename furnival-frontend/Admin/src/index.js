4e9ad66b8e0654eba0baaa65156f63a37407a35f
Ahmed Ramadan <ahmedmedo.am121212@gmail.com>
2023-08-04 16:17:21 +0300

    Init Routes

--- furnival-frontend/Admin/src/index.js
+++ furnival-frontend/Admin/src/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import RootLayout from './pages/RootLayout';
+import OverView from './components/OverView';
+import Products from './components/Products';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <OverView /> },
+      { path: 'home', element: <OverView /> },
+      { path: 'home/products', element: <Products /> },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
